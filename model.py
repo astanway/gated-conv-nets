9c6e1cbd7c6d8ed4d6856dba3791ed0bbd673c8d
Abe Stanway <abestanway@gmail.com>
2016-12-31 17:26:40 -0500

    Running

--- model.py
+++ model.py
@@ -11,7 +11,7 @@ corpus = "Smith served as the news editor for 1UP.com , the sister site to the n
 corpus = corpus.split(' ')
 
 # Placeholders for input, output and dropout
-sequence_length = 1
+sequence_length = 32
 embedding_size = 128
 vocabulary = set(corpus)
 vocab_mapping = {i:x for x, i in enumerate(vocabulary)}
@@ -22,135 +22,165 @@ layers = 128
 kernel_width = 4
 
 input_x = tf.placeholder(tf.int32, shape=(None, sequence_length), name="input_x")
-input_y = tf.placeholder(tf.int32, shape=(None, vocab_size), name="input_y")
+input_y = tf.placeholder(tf.int64, shape=(), name="input_y")
 
 # [batch, in_height, in_width, in_channels]
 input_shape = [None, sequence_length, embedding_size, 1]
 
-# [filter_height, filter_width, in_channels, out_channels]
-filter_shape = [2, embedding_size, 1, 1]
+
 
 # Embedding layer
-with tf.device('/cpu:0'), tf.name_scope("embedding"):
+Wembedding = tf.Variable(tf.random_uniform([vocab_size, embedding_size], -1.0, 1.0), name="Wembedding")
+embedded_words = tf.nn.embedding_lookup(Wembedding, input_x)
+embedded_words_expanded = tf.expand_dims(embedded_words, -1)
+#   padded = tf.Print(embedded_words_expanded, [embedded_words_expanded], summarize=1000)
 
-    W = tf.Variable(tf.random_uniform([vocab_size, embedding_size], -1.0, 1.0), name="Wembedding")
+def glu(filter_shape, paddings, layer_input, layer_name):
+    global padded
+    padded_input = tf.pad(layer_input, paddings, "CONSTANT") 
+    print "padded input", padded_input
+    #padded = tf.Print(padded_input, [padded_input], summarize=1000)
+
+    center_w = filter_shape[1] // 2 
+    mask = np.ones((filter_shape), dtype=np.float32)
+    mask[:, center_w+1: ,: ,:] = 0.
+
+    W = tf.Variable(tf.truncated_normal(filter_shape, stddev=1.0/filter_shape[2]), name="W%s" % layer_name)
+   # padded = tf.Print(W, [W], summarize=1000)
+    W *= tf.constant(mask, dtype=tf.float32) # mask the kernel
     
-    # (?, 20, 128)
-    embedded_words = tf.nn.embedding_lookup(W, input_x)
+    #padded = tf.Print(W, [W], summarize=1000)
     
-    # (?, 20, 128, 1)
-    embedded_words_expanded = tf.expand_dims(embedded_words, -1)
+    b = tf.Variable(tf.constant(1.0 / filter_shape[2], shape=[filter_shape[-1]]), name="b%s" % layer_name)
+    conv1 = tf.nn.conv2d(
+        padded_input,
+        W,
+        strides=[1, 1, 1, 1],
+        padding="VALID",
+        name="conv1")
 
-    # embedded_output = tf.nn.embedding_lookup(W, input_y)
-    # embedded_output_expanded = tf.expand_dims(embedded_output, -1)
+    conv1 = tf.nn.bias_add(conv1, b)
 
+    V = tf.Variable(tf.truncated_normal(filter_shape, stddev=1.0/filter_shape[2]), name="V%s" % layer_name)
+    c = tf.Variable(tf.constant(1.0 / filter_shape[2], shape=[filter_shape[-1]]), name="c%s" % layer_name)
+    conv2 = tf.nn.conv2d(
+        padded_input,
+        V,
+        strides=[1, 1, 1, 1],
+        padding="VALID",
+        name="conv2")
+    conv2 = tf.sigmoid(tf.nn.bias_add(conv2, c))
 
-W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.1), name="W")
-b = tf.Variable(tf.constant(0.1, shape=[1]), name="b")
-conv1 = tf.nn.conv2d(
-    embedded_words_expanded,
-    W,
-    strides=[1, 1, 1, 1],
-    padding="SAME", # todo: http://stackoverflow.com/questions/37659538/custom-padding-for-convolutions-in-tensorflow
-    name="conv1")
+    h = tf.multiply(conv1, conv2)
 
-conv1 = tf.nn.bias_add(conv1, b)
+    return h
+    
+# mask the kernels
+# do not zero pad. zero padding retains the same size
+# if mask_type is not None:
 
-V = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.1), name="V")
-c = tf.Variable(tf.constant(0.1, shape=[1]), name="c")
-conv2 = tf.nn.conv2d(
-    embedded_words_expanded,
-    V,
-    strides=[1, 1, 1, 1],
-    padding="SAME", # todo: http://stackoverflow.com/questions/37659538/custom-padding-for-convolutions-in-tensorflow
-    name="conv2")
-conv2 = tf.sigmoid(tf.nn.bias_add(conv2, c))
+# [filter_height, filter_width, in_channels, out_channels]
+# use rectangular kernels
+filter_shape = [3, 3, 1, 128]
+#paddings = [[0,0], [1, 0], [0, 0], [0, 0]]
+paddings = [[0,0], [0, 0], [0, 0], [0, 0]]
+h1 = glu(filter_shape, paddings, embedded_words_expanded, 1)
+print "first layer output", h1
 
-h = tf.multiply(conv1, conv2)
+filter_shape = [3, 3, 128, 128]
+#paddings = [[0,0], [0, 0], [0, 0], [0, 0]]
+h1a = glu(filter_shape, paddings, h1, 2)
+print "second_layer output", h1a
 
+filter_shape = [3, 3, 128, 256]
+#paddings = [[0,0], [0, 0], [0, 0], [0, 0]]
+h2 = glu(filter_shape, paddings, h1a, 2)
+print "second_layer output", h2
+
+filter_shape = [4, 4, 256, 512]
+#paddings = [[0,0], [0, 0], [0, 0], [0, 0]]
+h3 = glu(filter_shape, paddings, h2, 3)
+print "third layer output", h3
+
+filter_shape = [4, 4, 512, 512]
+#paddings = [[0,0], [0, 0], [0, 0], [0, 0]]
+h4 = glu(filter_shape, paddings, h3, 4)
+print "third layer output", h4
+
+filter_shape = [4, 10, 512, 1024]
+#paddings = [[0,0], [0, 0], [0, 0], [0, 0]]
+h5 = glu(filter_shape, paddings, h4, 5)
+print "third layer output", h5
+
+filter_shape = [4, 10, 1024, 1024]
+#paddings = [[0,0], [0, 0], [0, 0], [0, 0]]
+h6 = glu(filter_shape, paddings, h5, 6)
+print "third layer output", h6
+
+filter_shape = [4, 15, 1024, 1024]
+#paddings = [[0,0], [0, 0], [0, 0], [0, 0]]
+h7 = glu(filter_shape, paddings, h6, 7)
+print "third layer output", h7
+
+filter_shape = [4, 20, 1024, 1024]
+#paddings = [[0,0], [0, 0], [0, 0], [0, 0]]
+h8 = glu(filter_shape, paddings, h7, 8)
+print "third layer output", h8
+
+filter_shape = [4, 30, 1024, 1024]
+#paddings = [[0,0], [0, 0], [0, 0], [0, 0]]
+h9 = glu(filter_shape, paddings, h8, 9)
+print "third layer output", h9
+
+filter_shape = [5, 36, 1024, 1024]
+#paddings = [[0,0], [0, 0], [0, 0], [0, 0]]
+h10 = glu(filter_shape, paddings, h9, 10)
+print "third layer output", h10
+
+shaped = tf.squeeze(h10)
+print shaped
+#padded = tf.Print(shaped, [shaped], summarize=1000)
 # TODO: Add the residual every 5 units
 #if (i % 5 == 0):
 #    gated = tf.add(gated, embedded_words_expanded)
-
-# Activation
-h = tf.nn.relu(h, name="relu")
-
-#print h
-i = 1
-conv1 = tf.nn.conv2d(
-    h,
-    tf.Variable(tf.truncated_normal(filter_shape, stddev=0.1), name="W%s" % i),
-    strides=[1, 1, 1, 1],
-    padding="SAME", # todo: http://stackoverflow.com/questions/37659538/custom-padding-for-convolutions-in-tensorflow
-    name="conv1%s" % i)
-conv1 = tf.nn.bias_add(conv1, tf.Variable(tf.constant(0.1, shape=[1]), name="b%s" % i))
-
-conv2 = tf.nn.conv2d(
-    h,
-    tf.Variable(tf.truncated_normal(filter_shape, stddev=0.1), name="V%s" % i),
-    strides=[1, 1, 1, 1],
-    padding="SAME", # todo: http://stackoverflow.com/questions/37659538/custom-padding-for-convolutions-in-tensorflow
-    name="conv2%s" % i)
-conv2 = tf.sigmoid(tf.nn.bias_add(conv2, tf.Variable(tf.constant(0.1, shape=[1]), name="c%s" % i)))
-
-# TODO: Add the residual every 5 units
-#if (i % 5 == 0):
-#    gated = tf.add(gated, embedded_words_expanded)
-
-# Activation
-h = tf.nn.relu(tf.multiply(conv1, conv2), name="relu")
-
-# use a softmax layer on h to get the embedding of the next word
-
-# Final (unnormalized) scores and predictions
-# with tf.name_scope("output"):
-#     W = tf.get_variable(
-#         "W",
-#         shape=[num_units, vocab_size],
-#         initializer=tf.contrib.layers.xavier_initializer())
-#     b = tf.Variable(tf.constant(0.1, shape=[vocab_size]), name="b")
-#     scores = tf.nn.xw_plus_b(h_pool_flat, W, b, name="scores")
-
-# CalculateMean cross-entropy loss
-print h
-print input_y
-
-# softmax on h?
-#prediction = tf.nn.softmax(h)
-
+## need fully connected layer
 
 # Init weights, bioas, (all zeros first) 
-W = tf.Variable(tf.zeros([vocab_size, embedding_size]))
+softmax_w = tf.Variable(tf.zeros([vocab_size, 1024]), name="softmax_w")
 
-# define softmax function
-# first multiply x and w, then add b vector. apply softmax to get probabilities
-#y = tf.nn.softmax(tf.matmul(h, W))
+# softmax
+total = 0.0
+print vocab_size
+for i in range(0, vocab_size):
+    output_embedding = tf.nn.embedding_lookup(softmax_w, i)
+    print output_embedding
+    total += tf.exp(tf.reduce_sum(tf.mul(shaped, output_embedding))) # dot product
 
-total = 0
-for i in W:
-    total += exp(h * W[i])
+# why make the whole output? just make what you want.
+#output = [tf.exp(tf.reduce_sum(tf.mul(shaped, tf.nn.embedding_lookup(softmax_w, i)))) / total for i in range(0, vocab_size)]
+#output = tf.constant(output)
 
-divisor = 1 / total
-output = [exp(h * W[i]) / total for i in W]
+output = tf.exp(tf.reduce_sum(tf.mul(shaped, tf.nn.embedding_lookup(softmax_w, input_y)))) / total
 
-loss = 
-
-losses = tf.nn.softmax_cross_entropy_with_logits(h, input_y)
-loss = tf.reduce_mean(losses)
-#loss_summary = tf.summary.scalar('loss', loss)
+print output
+loss = -tf.log(output)
+loss_sum = tf.scalar_summary("loss", loss)
 train_step = tf.train.GradientDescentOptimizer(0.5).minimize(loss)
 
 sess = tf.InteractiveSession()
 tf.global_variables_initializer().run()
-for index in range(30, 300):
+for index in range(32, 300):
+    print index
     x = corpus[index - sequence_length:index]
     x = np.array(
     [
         [vocab_mapping[word] for word in x]
     ])
 
-    y = [0 for v in range(0, vocab_size)]
-    y[vocab_mapping[corpus[index + 1]]] = 1
-    y = np.array([y])
-    _, l = sess.run([train_step, loss], feed_dict={input_x: x, input_y: y})
-    print l
+    # y = [0 for v in range(0, vocab_size)]
+    # y[vocab_mapping[corpus[index + 1]]] = 1
+    # y = np.array([y])
+    writer = tf.train.SummaryWriter("log", sess.graph_def)
+    _, l = sess.run([train_step, loss_sum], feed_dict={input_x: x, input_y: vocab_mapping[corpus[index + 1]]})
+    write.add_summary(l)
+    print("%f" % l)
