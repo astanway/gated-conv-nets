9a077f64d74faecc460cdc02849da34ae59c13a6
Abe Stanway <abestanway@gmail.com>
2017-01-08 18:58:02 -0500

    More cleanup

--- model.py
+++ model.py
@@ -38,7 +38,7 @@ def get_data(test=False):
     lines = []
 
     # Open both train and test data so that they can share a unified vocabulary
-    with open('big.wiki.train.tokens') as f:
+    with open('wiki.train.tokens') as f:
         for line in f:
             line = "<S> " + line + " </S>"
             l = line.lower().strip().split()
@@ -48,7 +48,7 @@ def get_data(test=False):
                 for w in l:
                     vocabulary.add(w)
 
-    with open('big.wiki.test.tokens') as f:
+    with open('wiki.test.tokens') as f:
         for line in f:
             line = "<S> " + line + " </S>"
             l = line.lower().strip().split()
@@ -74,29 +74,23 @@ input_x = tf.placeholder(tf.int32, shape=(minibatch_size, sequence_length), name
 input_y = tf.placeholder(tf.float32, shape=(minibatch_size, sequence_length - 1), name="input_y")
 
 def init_vars():
-
-    # Embedding layer
-    all_word_embeddings= tf.Variable(tf.random_normal([vocab_size, embedding_size], stddev=.01), name="all_word_embeddings")
-    input_embeddings = tf.nn.embedding_lookup(all_word_embeddings, input_x)
-    input_embeddings_expanded = tf.expand_dims(input_embeddings, 1) # give it height of 1
-
-    def glu(filter_shape, layer_input, layer_name, res=False, last_layer=False):
+    def glu(kernel_shape, layer_input, layer_name):
         # Pad the left side to prevent kernels from viewing future context
-        kernel_width = filter_shape[1]
+        kernel_width = kernel_shape[1]
         left_pad = kernel_width - 1
         paddings = [[0,0],[0,0],[left_pad,0],[0,0]]
         padded_input = tf.pad(layer_input, paddings, "CONSTANT")
 
         # Idea: kernel masking instead of padding input?
         # mask the kernel to future words from leaking into the kernel
-        #center_w = filter_shape[1] // 2
-        #mask = np.ones((filter_shape), dtype=np.float32)
+        #center_w = kernel_shape[1] // 2
+        #mask = np.ones((kernel_shape), dtype=np.float32)
         #mask[:, center_w+1: ,: ,:] = 0.
         #W *= tf.constant(mask, dtype=tf.float32)
 
         # First convolutional layer
-        W = tf.Variable(tf.random_normal(filter_shape, stddev=np.sqrt(2.0 / (filter_shape[0] * filter_shape[1]))), name="W%s" % layer_name)
-        b = tf.Variable(tf.zeros(shape=[filter_shape[2] * filter_shape[3]]), name="b%s" % layer_name)
+        W = tf.Variable(tf.random_normal(kernel_shape, stddev=np.sqrt(2.0 / (kernel_shape[0] * kernel_shape[1]))), name="W%s" % layer_name)
+        b = tf.Variable(tf.zeros(shape=[kernel_shape[2] * kernel_shape[3]]), name="b%s" % layer_name)
         conv1 = tf.nn.depthwise_conv2d(
             padded_input,
             W,
@@ -106,8 +100,8 @@ def init_vars():
         conv1 = tf.nn.bias_add(conv1, b)
 
         # Gating sigmoid layer
-        V = tf.Variable(tf.random_normal(filter_shape, stddev=np.sqrt(2.0 / (filter_shape[0] * filter_shape[1]))), name="V%s" % layer_name)
-        c = tf.Variable(tf.zeros(shape=[filter_shape[2] * filter_shape[3]]), name="c%s" % layer_name)
+        V = tf.Variable(tf.random_normal(kernel_shape, stddev=np.sqrt(2.0 / (kernel_shape[0] * kernel_shape[1]))), name="V%s" % layer_name)
+        c = tf.Variable(tf.zeros(shape=[kernel_shape[2] * kernel_shape[3]]), name="c%s" % layer_name)
         conv2 = tf.nn.depthwise_conv2d(
             padded_input,
             V,
@@ -118,51 +112,7 @@ def init_vars():
 
         h = tf.multiply(conv1, conv2)
 
-        # residual layer - add input to outpu
-        if res:
-            h = tf.add(h, padded_input)
-
         return h
-
-    # [filter_height, filter_width, in_channels, out_channels]
-    filter_shape = [1, 3, embedding_size, 1]
-    h1 = glu(filter_shape, input_embeddings_expanded, 1)
-
-    filter_shape = [1, 3, 128, 1]
-    h1a = glu(filter_shape, h1, 2)
-
-    filter_shape = [1, 3, 128, 1]
-    h2 = glu(filter_shape, h1a, 2)
-
-    filter_shape = [1, 5, 128, 2]
-    h3 = glu(filter_shape, h2, 3)
-
-    filter_shape = [1, 5, 256, 2]
-    h4 = glu(filter_shape, h3, 4)
-
-    # filter_shape = [4, 128, 1, 128]
-    # h5 = glu(filter_shape, paddings, h4, 5)
-    #
-    # filter_shape = [4, 128, 1, 128]
-    # h6 = glu(filter_shape, paddings, h5, 6)
-
-    # filter_shape = [4, 128, 1, 128]
-    # h7 = glu(filter_shape, paddings, h6, 7)
-    #
-    # filter_shape = [4, 128, 1, 128]
-    # h8 = glu(filter_shape, paddings, h7, 8)
-    #
-    # filter_shape = [4, 128, 1, 128]
-    # h9 = glu(filter_shape, paddings, h8, 9)
-    # h9 = tf.add(h8, h9) # residual layer
-
-    output_embedding_size = 1024
-    filter_shape = [1, 5, 512, 2]
-    h10 = glu(filter_shape, h4, 10, last_layer=True)
-    # Remove the last element, as the next word is in a new sequence and we do not predict it
-    # Todo: this isn't really right
-    h10 = tf.slice(h10, [0, 0, 0, 0], [-1, -1, sequence_length-1, -1])
-    h10 = tf.squeeze(h10)
 
     def compute_sampled_softmax(hidden):
         """ Compute sampled softmax for training"""
@@ -172,49 +122,65 @@ def init_vars():
         losses = tf.nn.sampled_softmax_loss(output_embedding, output_bias, hidden, labels, 25, vocab_size, num_true=1, remove_accidental_hits=True, partition_strategy='mod', name='sampled_softmax_loss')
         return losses
 
-    def compute_full_softmax(word):
-        # Extract the label that we sent earlier
-        label = tf.cast(word[-1], tf.int32)
-        word = tf.slice(word, [0], [embedding_size])
+    # Embedding layer
+    all_word_embeddings = tf.Variable(tf.random_normal([vocab_size, embedding_size], stddev=.01), name="all_word_embeddings")
+    input_embeddings = tf.nn.embedding_lookup(all_word_embeddings, input_x)
+    input_embeddings_expanded = tf.expand_dims(input_embeddings, 1) # give it height of 1
 
-        # Calculate total
-        word = tf.transpose(tf.expand_dims(word, 1))
-        total = tf.reduce_sum(tf.map_fn(lambda output_slice: tf.exp(tf.matmul(word, tf.expand_dims(output_slice, 1))), output_embedding))
+    # [height, width, in_channels, out_channels]
+    kernel_shape = [1, 3, embedding_size, 1]
+    h0 = glu(kernel_shape, input_embeddings_expanded, 1)
 
-        # Get the probability of the labeled word.
-        output_slice = tf.nn.embedding_lookup(output_embedding, label)
-        labeled_prob = tf.exp(tf.reduce_sum(tf.matmul(word, tf.expand_dims(output_slice, 1)))) / total
+    kernel_shape = [1, 3, 128, 1]
+    h1 = glu(kernel_shape, h0, 2)
 
-        #### FOR GENERATION WITHOUT LABELS #####
-        # Get individual probabilities per word
-        # probs = tf.map_fn(lambda output_slice: tf.exp(tf.reduce_sum(tf.matmul(word, tf.expand_dims(output_slice, 1)))) / total, output_embedding)
+    kernel_shape = [1, 3, 128, 1]
+    h2 = glu(kernel_shape, h1, 3)
 
-        # Make sure they add to 1
-        # sum_probs = tf.reduce_sum(probs)
+    h2 = tf.add(h2, h0) # skip two residual layer
 
-        # Word with highest probability is predicted word
-        # predicted_word = tf.argmax(probs, 0)
+    kernel_shape = [1, 5, 128, 2]
+    h3 = glu(kernel_shape, h2, 4)
 
-        # Get the probability of the predicted word
-        # predicted_word_prob = probs[tf.cast(predicted_word, tf.int32)]
+    kernel_shape = [1, 5, 256, 1]
+    h4 = glu(kernel_shape, h3, 5)
 
-        return -tf.log(labeled_prob)
+    kernel_shape = [1, 5, 256, 1]
+    h5 = glu(kernel_shape, h4, 6)
 
+    h5 = tf.add(h5, h3) # skip two residual layer
+
+    kernel_shape = [1, 5, 256, 2]
+    h6 = glu(kernel_shape, h5, 7)
+
+    kernel_shape = [1, 5, 512, 2]
+    last_hidden = glu(kernel_shape, h6, 8)
+
+    # Output word embeddings. Note: these are not the same as the input word embeddings.
+    output_embedding_size = kernel_shape[2] * kernel_shape[3]
     output_embedding = tf.Variable(tf.random_normal([vocab_size, output_embedding_size], stddev=.001), name="output_embedding")
     output_bias = tf.Variable(tf.zeros([vocab_size]), name="output_bias")
 
-    # Concate the labels onto the context index and send off to be evaluated
-    concated = tf.concat(2, [h10, tf.expand_dims(input_y, 2)])
+    # Remove the last element, as the next word is in a new sequence and we do not predict it
+    last_hidden = tf.slice(last_hidden, [0, 0, 0, 0], [-1, -1, sequence_length-1, -1])
+    last_hidden = tf.squeeze(last_hidden)
 
+    # Concat the labels onto the context index and send off to be evaluated
+    concated = tf.concat(2, [last_hidden, tf.expand_dims(input_y, 2)])
+
+    # Evaluate losses with a sammpled softmax
     losses = tf.map_fn(lambda sequence: compute_sampled_softmax(sequence), concated)
     loss = tf.reduce_mean(losses)
 
+    # Find the perplexity
     sum_losses = tf.map_fn(lambda sequence_loss: tf.reduce_sum(sequence_loss), losses)
     batch_perplexities = tf.map_fn(lambda sum_loss: tf.exp(1.0/sequence_length) * sum_loss, sum_losses)
     perplexity = tf.reduce_mean(batch_perplexities)
     p = tf.Print(perplexity, [perplexity], summarize=5000, message="perplexity")
     l = tf.Print(loss, [loss], summarize=5000, message="loss")
 
+    # If we are training a model, proceed to optimize gradients and backprop.
+    # Gradient clipping set to -.1, .1.
     if train:
         optimizer = tf.train.MomentumOptimizer(.5, .99)
         gvs = optimizer.compute_gradients(loss)
